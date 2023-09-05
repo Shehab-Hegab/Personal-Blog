c6699ee94a59f5555dc0a1e836e0743d15ac94c1
Shehab-Hegab <137138481+Shehab-Hegab@users.noreply.github.com>
2023-09-06 02:22:31 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Smooth scrolling for anchor links in the navigation menu
+$(document).ready(function () {
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
